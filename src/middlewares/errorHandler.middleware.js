560229f38bf234d3b8885a064220f60d38c92c05
Lucas Freitas <devlucao@gmail.com>
2026-02-09 16:08:46 -0300

    fix(error handler bug): fix error handler bug
    Fix error handler status bug and error message of Internal server error.

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,17 +1,15 @@
 const { AppError } = require("../errors/AppError");
 
-const errorHandler = (err, req, res, next) => {
-  const status = err.statusCode || 500;
+const errorHandler = (err, _req, res, _next) => {
+  const status = Number.isInteger(err.statusCode) ? err.statusCode : 500;
 
-  if(err instanceof AppError) {
-    return res.status(err.statusCode).json({ error: err.message });
+  if (err instanceof AppError) {
+    return res.status(status).json({ error: err.message });
   }
 
   console.error(err);
 
-  return res.status(status).json({
-    error: err.message || "Internal server error."
-  });
+  return res.status(status).json({ error: "Internal server error." });
 }
 
 module.exports = { errorHandler };
